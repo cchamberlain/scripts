6c2330ea8727d8e20b4aaa5d2569bc193179b983
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-11-17 00:39:35 -0800

    pod updates

--- Modules/Pod/Pod.psm1
+++ Modules/Pod/Pod.psm1
@@ -406,6 +406,92 @@ function SavePodfile {
   }
 }
 
+function Update-NuGet {
+  [CmdletBinding(
+    SupportsShouldProcess=$TRUE,
+    ConfirmImpact="Medium"
+  )]
+  PARAM(
+    [parameter(Position=0,Mandatory=$TRUE,HelpMessage="Directory to install / update NuGet.")]
+    [alias("r")]
+    [string]$NUGET_ROOT="$env:APPDATA/NuGet"
+  )
+  PROCESS {
+    $sourceNugetExe = "https://nuget.org/nuget.exe"
+    $targetNugetExe = "$NUGET_ROOT\nuget.exe"
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+    Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+  }
+}
+
+function ConvertTo-Base64
+{
+    param
+    (
+        [string] $SourceFilePath,
+        [string] $TargetFilePath
+    )
+
+    $SourceFilePath = Resolve-PathSafe $SourceFilePath
+    $TargetFilePath = Resolve-PathSafe $TargetFilePath
+
+    $bufferSize = 9000 # should be a multiplier of 3
+    $buffer = New-Object byte[] $bufferSize
+
+    $reader = [System.IO.File]::OpenRead($SourceFilePath)
+    $writer = [System.IO.File]::CreateText($TargetFilePath)
+
+    $bytesRead = 0
+    do
+    {
+        $bytesRead = $reader.Read($buffer, 0, $bufferSize);
+        $writer.Write([Convert]::ToBase64String($buffer, 0, $bytesRead));
+    } while ($bytesRead -eq $bufferSize);
+
+    $reader.Dispose()
+    $writer.Dispose()
+}
+
+function ConvertFrom-Base64
+{
+    param
+    (
+        [string] $SourceFilePath,
+        [string] $TargetFilePath
+    )
+
+    $SourceFilePath = Resolve-PathSafe $SourceFilePath
+    $TargetFilePath = Resolve-PathSafe $TargetFilePath
+
+    $bufferSize = 9000 # should be a multiplier of 4
+    $buffer = New-Object char[] $bufferSize
+
+    $reader = [System.IO.File]::OpenText($SourceFilePath)
+    $writer = [System.IO.File]::OpenWrite($TargetFilePath)
+
+    $bytesRead = 0
+    do
+    {
+        $bytesRead = $reader.Read($buffer, 0, $bufferSize);
+        $bytes = [Convert]::FromBase64CharArray($buffer, 0, $bytesRead);
+        $writer.Write($bytes, 0, $bytes.Length);
+    } while ($bytesRead -eq $bufferSize);
+
+    $reader.Dispose()
+    $writer.Dispose()
+}
+
+function Resolve-PathSafe
+{
+    param
+    (
+        [string] $Path
+    )
+
+    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
+}
+
+
 function New-Podfile {
   [CmdletBinding(
     SupportsShouldProcess=$TRUE,
